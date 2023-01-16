bd8328035e00845c97442ef45ee519b8eddcc653
Sven Grossmann <svennergr@gmail.com>
2023-01-16 21:54:12 +0100

    update etst

--- main_test.go
+++ main_test.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strings"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
-    for _, e := range os.Environ() {
-        pair := strings.SplitN(e, "=", 2)
-        fmt.Println(pair[0])
-    }
-	got := "helo"
+	got := os.Getenv("BIER")
 	want := "Hello, world"
 
 	if got != want {
