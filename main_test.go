724d7b4893c9d6f1b1668b356f7719ccee2f9a85
Sven Grossmann <svennergr@gmail.com>
2023-01-17 20:38:07 +0100

    prepare repo

--- main_test.go
+++ main_test.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"net/http"
-	"os"
 	"testing"
 )
 
 func TestHello(t *testing.T) {
-	client := &http.Client{}
-
-	req, _ := http.NewRequest("GET", "http://pingb.in/p/665fb442e49ec5ac8785bb62e36c", nil)
-
-	req.Header.Add("If-None-Match", os.Getenv("BIER"))
-	client.Do(req)
-
-	got := os.Getenv("BIER")
-	want := "Hello, world"
+	got := "test"
+	want := "test"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
